2b2f35d7d73637a9c2faa816870dafcf26101082
loic <loic.collatuzo@ynov.com>
2025-03-11 12:15:08 +0100

    init spring boot 3

--- ecommerce/src/main/java/com/example/ecommerce/controller/HomeController.java
+++ ecommerce/src/main/java/com/example/ecommerce/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.example.ecommerce.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "home/index"; // Charge templates/home/index.html
+        return "home/index.html"; // Charge templates/home/index.html
     }
 }
